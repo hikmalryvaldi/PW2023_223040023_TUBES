3e5e6f22b9b6fe128ac7aac4215d1b032b888156
hikmalryvaldi <hikmalryvaldi@gmail.com>
2023-06-01 13:09:34 +0700

    new login simple

--- Account/Login.php
+++ Account/Login.php
@@ -6,12 +6,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+
+    <!-- FONT AWS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../css/account/login.css">
 </head>
 
 <body>
 
+    <div class="container">
+        <div class="card">
+            <h1><span>Lo</span>gin</h1>
 
+            <form action="" method="post">
+                <div class="email">
+                    <label for="email"><i class="fa-regular fa-envelope"></i></label>
+                    <input type="email" name="email" id="email" require autofocus placeholder="Enter your email" autocomplete="0">
+                </div>
+
+                <div class="pass">
+                    <label for="password"><i class="fa-solid fa-key"></i></label>
+                    <input type="password" name="password" id="password" require placeholder="Enter your password">
+                    <i class="fa-solid fa-eye" id="eye"></i>
+                </div>
+
+                <div class="buttLog">
+                    <button type="submit" name="login">Login Now</button>
+                    <p>Don't have an account? <a href="sigup.php">Sigup Now</a></p>
+                </div>
+
+            </form>
+        </div>
+    </div>
 
 </body>
+
+<script src="../js/passEye.js"></script>
 
 </html>
